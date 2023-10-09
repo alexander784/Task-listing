814b59f59078c3f3c8033aed501440add52bfa2d
alexander <alexandernyaga1999@gmail.com>
2023-10-09 19:32:57 +0300

    add Eventlistener

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const taskInput = document.getElementById("task");
+    const addTaskButton = document.getElementById("add-task");
+    const taskList = document.getElementById("task-list");
+
+});
