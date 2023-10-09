72127e40e260e1bc67e881c088bdf44dd1c99f8a
alexander <alexandernyaga1999@gmail.com>
2023-10-09 19:34:50 +0300

    add taskButton

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ document.addEventListener("DOMContentLoaded", function () {
     const addTaskButton = document.getElementById("add-task");
     const taskList = document.getElementById("task-list");
 
+    addTaskButton.addEventListener("click", function () {
+        const taskText = taskInput.ariaValueMax.trim();
+
+        
+    })
+
 });
